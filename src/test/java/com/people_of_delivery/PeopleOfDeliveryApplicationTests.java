6336dfa003e7f70ca5474192c8eeb6ef1290cbc7
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2022-04-08 23:19:57 +0900

    초기환경세팅

---
+++ src/test/java/com/people_of_delivery/PeopleOfDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people_of_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleOfDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
